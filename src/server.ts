80222f7680289e28d6095d550050384f3e4c98e5
Cash-Codes <sf4cashley@gmail.com>
2026-04-09 13:37:52 +0100

    feat: add sqllite persistence for orchestration runs

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import { config } from "./config/env.js";
+import { initDb } from "./db/index.js";
 import app from "./app.js";
+
+initDb();
 
 app.listen(config.PORT, () => {
   console.log(`Server listening on port ${config.PORT}`);
